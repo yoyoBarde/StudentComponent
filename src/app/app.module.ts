c05a480fec80e5ac337d4ebb1c8c5656cc0d08c9
Julius Barde <yam1434414344@gmail.com>
2017-08-24 17:05:05 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/Forms';
+import { AppComponent } from './app.component';
+import { EntryComponent } from './entry/entry.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EntryComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
